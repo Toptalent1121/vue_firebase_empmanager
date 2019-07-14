b8b510e55b01f2cf24b2d5dbe5aae75f259b62f9
ura novic <urayoujing@gmail.com>
2019-07-15 01:53:04 +0900

    init commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBSaZZu9SKxeeTGHnzoFaEajTmqu4Hwlfc",
+    authDomain: "vuefs-prod-524e6.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-524e6.firebaseio.com",
+    projectId: "vuefs-prod-524e6",
+    storageBucket: "vuefs-prod-524e6.appspot.com",
+    messagingSenderId: "822766137280",
+    appId: "1:822766137280:web:1303e961fdfea5c6"
+}
